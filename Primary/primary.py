e8d091f2d86bf8c848b42724b3490e3de77115e1
Mykola Vorobey <34463351+NVorobey@users.noreply.github.com>
2021-10-15 14:46:56 +0300

    Add multithreading and delay on secondary

--- Primary/primary.py
+++ Primary/primary.py
@@ -1,31 +1,58 @@
 from flask import Flask, request
+from message_container import MessageContainer
 import requests
-from .message_container import MessageContainer
+import threading
 
 app = Flask(__name__)
 
 msg_container = MessageContainer()
 
 # primary port = 5000
-secondary_frst_endpoint = 'http://localhost:6000/messages'
-secondary_scnd_endpoint = 'http://localhost:7000/messages'
+endpoints = list()
+endpoints.append('http://localhost:6000/messages')
+endpoints.append('http://localhost:7000/messages')
+ReplicateCount = 0
 
+def ReplicateMessage(secondaryEndpoint, msg):
+    global ReplicateCount
+    resultOfResponce = requests.post(secondaryEndpoint, json=msg)
+    if (resultOfResponce.ok == True): ReplicateCount = ReplicateCount + 1
+
+def ProcessRequest(MsgTest):
+    global ReplicateCount
+    ReplicateCount = 0
+    treads = list()
+    for endpoint in endpoints:
+        treads.append(threading.Thread(target=ReplicateMessage, args=(endpoint, MsgTest)))
+
+    for t in treads:
+        t.start()
+
+    for t2 in treads:
+        t2.join()
+
+    return ReplicateCount
+
+
+@app.route('/')
+def index():
+    return "hello world"
 
 @app.route('/messages', methods=['POST'])
 def save_msg():
     msg = request.get_json()
-    r_frst = requests.post(secondary_frst_endpoint, json=msg)
-    r_scnd = requests.post(secondary_scnd_endpoint, json=msg)
-    if r_frst.status_code == 201 & r_scnd.status_code == 201:
+    port = request.server[1]
+    res = ProcessRequest(msg)
+
+    if res == len(endpoints):
         msg_container.append(msg["message"])
-    # Secondary nodes will have the same code
-    # but they will only have request.get_json() and append
 
-    # to test that the replication is blocking, introduce the delay/sleep on the Secondary
-    # you can use time.sleep(5) to do that
     return 'New message successfully added', 201
-
 
 @app.route('/messages', methods=['GET'])
 def return_msg():
     return msg_container.get_all(), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- secondary_frst/secondary.py
+++ secondary_frst/secondary.py
@@ -1,8 +1,34 @@
-import http.client
+from flask import Flask, request
+from message_container import MessageContainer
+import requests
+import random
+import time
 
-connection = http.client.HTTPSConnection("www.journaldev.com")
-connection.request("GET", "/")
-response = connection.getresponse()
-print("Status: {} and reason: {}".format(response.status, response.reason))
+app = Flask(__name__)
 
-connection.close()
+msg_container = MessageContainer()
+
+# primary port = 5000
+
+
+@app.route('/')
+def index():
+    return "hello world from secondary"
+
+@app.route('/messages', methods=['POST'])
+def save_msg():
+    msg = request.get_json()
+
+    delay = random.randint(1, 10)
+    time.sleep(delay)
+    msg_container.append(msg["message"])
+
+    return 'New message successfully added to secondary', 201
+
+@app.route('/messages', methods=['GET'])
+def return_msg():
+    return msg_container.get_all(), 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
