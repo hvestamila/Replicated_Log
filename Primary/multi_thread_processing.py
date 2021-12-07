f7a7be774462061f311edca97376c5fdbb14ec61
Mykola Vorobey <34463351+NVorobey@users.noreply.github.com>
2021-12-07 12:06:20 +0200

    Add quorum +Code refactoring

--- Primary/multi_thread_processing.py
+++ Primary/multi_thread_processing.py
@@ -10,21 +10,28 @@ SECONDARY_SCND = os.getenv('SECONDARY_SCND_BASE_PATH', 'http://localhost:5002')
 
 executor = concurrent.futures.ThreadPoolExecutor()
 
-
 class MultiThreadProcessing:
 
     def __init__(self):
-        self.endpoints = [SECONDARY_FRST + '/messages', SECONDARY_SCND + '/messages']
-        self.health = {SECONDARY_FRST:'suspected',
-                    SECONDARY_SCND:'suspected'}
+        self.endpoints = [SECONDARY_FRST, SECONDARY_SCND ]
+        self.health = {SECONDARY_FRST: 'suspected',
+                    SECONDARY_SCND: 'suspected'}
+
+    def isQuorum(self):
+        totalHealty = 0
+        for url in self.endpoints:
+            if self.health.get(url) == 'healthy':
+                totalHealty += 1
+
+        return (int(len(self.endpoints) / 2)) <= totalHealty
 
     def deliver_message(self, url, json, logger):
         try:
-            if self.health.get(url.replace('/messages', '')) == 'healthy':
-                response = requests.post(url=url, json=json)
+            if self.health.get(url) == 'healthy':
+                response = requests.post(url=url + '/messages', json=json, timeout=12)
             else:
-                raise HealthError(url.replace('/messages', ''),
-                 self.health.get(url.replace('/messages', '')))
+                logger.error(f"Health of {url} is {self.health.get(url)}")
+                return False
         except Exception as err:
             logger.error(f'URL: {url}; Exception: {repr(err)}')
             return False
@@ -36,7 +43,8 @@ class MultiThreadProcessing:
         while not self.deliver_message(url=url_secondary, json=message, logger=logger):
             logger.info(f'Host is unreachable: {url_secondary}; Waiting for {delay_time} seconds')
             time.sleep(delay_time)
-            delay_time = delay_time * 2
+            if delay_time < 8:
+                delay_time = delay_time * 2
         return True
 
     def replicate_message(self, msg_id, msg, w, logger):
@@ -63,24 +71,32 @@ class MultiThreadProcessing:
 
 
     def health_getter(self, endpoint):
-        return requests.get(endpoint+'/health').status_code
+        try:
+            return requests.get(endpoint+'/health', timeout=2).ok
+        except:
+            return False
 
 
     def health_process(self, logger, endpoint):
         bad_requests_count = 6
         while True:
-            try:
-                status = self.health_getter(endpoint=endpoint)
+            if self.health_getter(endpoint=endpoint):
                 if bad_requests_count > 0:
                     bad_requests_count -= 1
-            except Exception:
+            else:
                 if bad_requests_count < 12:
                     bad_requests_count += 1
-                status = 400
-            if bad_requests_count == 12:
+
+            if bad_requests_count >= 12:
                 self.health_setter(endpoint, 'unhealthy', logger)
-            elif bad_requests_count == 6:
+            elif bad_requests_count >= 6:
                 self.health_setter(endpoint, 'suspected', logger)
-            elif bad_requests_count == 0:
+            else:
                 self.health_setter(endpoint, 'healthy', logger)
             time.sleep(1)
+
+    def runHeardbeat(self, logger):
+        for url in self.endpoints:
+            executor.submit(self.health_process,
+                            endpoint=url,
+                            logger=logger)
